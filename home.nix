33c61bac3f94c27e705626b0592f496498f8f7d0
ortoklas <coolhand41169@gmail.com>
2026-07-25 11:17:43 -0500

    Add udiskie USB automounting

--- home.nix
+++ home.nix
@@ -1,35 +1,53 @@
-{ config, pkgs, ... }:
-
 {
 
-  imports = [
-    ./modules/packages.nix
-    ./modules/fastfetch.nix
-    ./modules/kitty.nix
-    ./modules/fonts.nix
-    ./modules/hyprland.nix
-    ./modules/keybindings.nix
-    ./modules/quickshell.nix
-    ./modules/swaync.nix
-  ];
+imports = [
+./modules/packages.nix
+./modules/fastfetch.nix
+./modules/kitty.nix
+./modules/fonts.nix
+./modules/hyprland.nix
+./modules/keybindings.nix
+./modules/quickshell.nix
+./modules/swaync.nix
+];
 
-  home.username = "highv";
-  home.homeDirectory = "/home/highv";
+home.username = "highv";
+home.homeDirectory = "/home/highv";
 
-  home.stateVersion = "24.05";
+home.stateVersion = "24.05";
 
-  home.sessionVariables = {
-    BROWSER = "brave";
-    TERMINAL = "kitty";
-  };
+home.sessionVariables = {
+BROWSER = "brave";
+TERMINAL = "kitty";
+};
 
-  programs.kitty = {
+services.udiskie = {
   enable = true;
+  automount = true;
+  notify = true;
+  tray = "never";
+
   settings = {
-    confirm_os_window_close = 0;
+    program_options = {
+      file_manager = "dolphin";
+      terminal = "kitty";
     };
   };
+};
 
+xdg.mimeApps = {
+enable = true;
+defaultApplications = {
+"inode/directory" = [ "org.kde.dolphin.desktop" ];
+};
+};
 
-  programs.home-manager.enable = true;
+programs.kitty = {
+enable = true;
+settings = {
+confirm_os_window_close = 0;
+};
+};
+
+programs.home-manager.enable = true;
 }
